fa37b89b009e7cedd7b519f5956ca5c048d06651
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2021-09-14 15:02:32 -0400

    Init

--- labs/lab0-setup/capture_audio.py
+++ labs/lab0-setup/capture_audio.py
@@ -1,10 +1,13 @@
+import numpy as np
 
 import sounddevice as sd
 from scipy.io.wavfile import write
 
 fs = 44100  # Sample rate
 seconds = 3  # Duration of recording
+sd.default.samplerate = fs
 
 myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
 sd.wait()  # Wait until recording is finished
-write('output.wav', fs, myrecording)  # Save as WAV file 
+
+write('output.wav', fs, myrecording)  # Save as WAV file
--- labs/lab0-setup/capture_image.py
+++ labs/lab0-setup/capture_image.py
@@ -1,10 +1,10 @@
-
 import cv2, os, sys
 
-def gstreamer_pipeline(capture_width=1280, capture_height=720, 
+
+def gstreamer_pipeline(capture_width=1280, capture_height=720,
                        display_width=1280, display_height=720,
                        framerate=60, flip_method=0):
-  return (
+    return (
         "nvarguscamerasrc ! "
         "video/x-raw(memory:NVMM), "
         f"width=(int){capture_width}, height=(int){capture_height}, "
@@ -15,24 +15,23 @@ def gstreamer_pipeline(capture_width=1280, capture_height=720,
         "video/x-raw, format=(string)BGR ! appsink"
     )
 
-HEIGHT=1280
-WIDTH=1920
+
+HEIGHT = 1280
+WIDTH = 1920
 center = (WIDTH / 2, HEIGHT / 2)
 M = cv2.getRotationMatrix2D(center, 180, 1.0)
 
-nano = False
+nano = True
 if nano:
-  cam = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
+    cam = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
 else:
-  # Start Camera
-  cam = cv2.VideoCapture(0)
-  cam.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)  # 3280
-  cam.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT) # 2464
-
-
+    # Start Camera
+    cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)  # 3280
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)  # 2464
 
 if cam.isOpened():
-  val, img = cam.read()
-  if val:
-    cv2.imwrite('output.png', img)
-    #cv2.imwrite('output.png', cv2.warpAffine(img, M, (WIDTH, HEIGHT)))
+    val, img = cam.read()
+    if val:
+        cv2.imwrite('output.png', img)
+        # cv2.imwrite('output.png', cv2.warpAffine(img, M, (WIDTH, HEIGHT)))

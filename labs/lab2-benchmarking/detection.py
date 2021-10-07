6045f5207a42a3709a11a6a4c1bca4e989578d44
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2021-10-06 21:16:33 -0400

    detection.py

--- labs/lab2-benchmarking/detection.py
+++ labs/lab2-benchmarking/detection.py
@@ -5,32 +5,35 @@ from timeit import default_timer as timer
 
 
 def main():
-    device = torch.device("cuda:0")
-    model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(
-            pretrained=True).cuda(device)
-    # model = torchvision.models.detection.ssdlite320_mobilenet_v3_large(pretrained=True).cuda(device)
-    num_params = sum([np.prod(p.size()) for p in model.parameters()])
-    print(num_params)
+    for device_str in ["cuda:0", "cpu"]:
+        print(device_str)
+        device = torch.device(device_str)
+        for model in [torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(
+                pretrained=True).to(device),
+                      torchvision.models.detection.ssdlite320_mobilenet_v3_large(
+                              pretrained=True).to(device)]:
+            print(model.__class__)
+            num_params = sum([np.prod(p.size()) for p in model.parameters()])
+            print(num_params)
 
-    model.eval()
-    x = [torch.rand(3, 224, 224, device=device)]
-    for _ in range(10):
-        predictions = model(x)
-    # heat up
+            model.eval()
+            x = [torch.rand(3, 224, 224, device=device)]
+            for _ in range(10):
+                predictions = model(x)
+            # heat up
 
-    time = np.zeros(10)
+            time = np.zeros(10)
+            # for size in [224, 240, 260, 300, 380, 456, 528, 600]:
+            for size in [224]:
+                x = [torch.rand(3, size, size, device=device)]
+                for i in range(10):
+                    start_time = timer()
+                    predictions = model(x)
+                    end_time = timer()
+                    time[i] = end_time - start_time
 
-    for size in [224, 240, 260, 300, 380, 456, 528, 600]:
-        x = [torch.rand(3, size, size, device=device)]
-        for i in range(10):
-            start_time = timer()
-            predictions = model(x)
-            end_time = timer()
-            time[i] = end_time - start_time
-            start_time = end_time
-
-        print('Size,', size, 'Mean:', time.mean())
-        print('Standard Deviation:', time.std())
+                print('Size,', size, 'Mean:', time.mean())
+                print('Standard Deviation:', time.std())
 
 
 if __name__ == '__main__':
@@ -71,3 +74,23 @@ if __name__ == '__main__':
 # Standard Deviation: 0.006131127774616639
 # Size, 600 Mean: 0.1068273856988526
 # Standard Deviation: 0.0009570182228357254
+
+# on desktop gpu/cpu
+# cuda:0
+# <class 'torchvision.models.detection.faster_rcnn.FasterRCNN'>
+# 19386354
+# Size, 224 Mean: 0.01445891167732212
+# Standard Deviation: 5.880855513996412e-05
+# <class 'torchvision.models.detection.ssd.SSD'>
+# 3440060
+# Size, 224 Mean: 0.0719202999263873
+# Standard Deviation: 0.00041043049684485447
+# cpu
+# <class 'torchvision.models.detection.faster_rcnn.FasterRCNN'>
+# 19386354
+# Size, 224 Mean: 0.05502555492387997
+# Standard Deviation: 0.00199144965521597
+# <class 'torchvision.models.detection.ssd.SSD'>
+# 3440060
+# Size, 224 Mean: 0.07409234972601268
+# Standard Deviation: 0.0023820226879700908

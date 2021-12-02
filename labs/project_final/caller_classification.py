15dc67d1b3b717e0f8c37c75e37670e71996a98b
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2021-12-02 11:43:48 -0500

    Update Pipeline

--- labs/project_final/caller_classification.py
+++ labs/project_final/caller_classification.py
@@ -3,6 +3,7 @@ from classification import *
 
 class Classify:
     def __init__(self):
+        print('Start Loading')
         params = ParamsClassification(B=64, lr=1e-3, verbose=False,
                                       device='cuda:0', flip=False,
                                       normalize=False,
@@ -11,6 +12,7 @@ class Classify:
         model = MobileNetV3Small_All(params)
         self.learner = Learning(params, model)
         self.learner.load_model(epoch=2)
+        print('Loading Complete')
 
     def __call__(self):
         self.learner.test()

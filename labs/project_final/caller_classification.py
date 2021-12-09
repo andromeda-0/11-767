208270a10a699c159281b27f2f916b64aea20329
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2021-12-09 16:00:18 -0500

    Resolve warnings

--- labs/project_final/caller_classification.py
+++ labs/project_final/caller_classification.py
@@ -8,10 +8,10 @@ class Classify:
                                       device='cpu', flip=False,
                                       normalize=False,
                                       data_root='/home/zongyuez/data/In-the-wild-224',
-                                      resize=-1)
+                                      resize=32)
         model = MobileNetV3Small_All(params)
         self.learner = Learning(params, model)
-        self.learner.load_model(epoch=2, name='MobileNetV3Small_All_class_b=64lr=0.001_r224')
+        self.learner.load_model(epoch=8, name='MobileNetV3Small_All_class_b=64lr=0.001_r32')
         self.learner._load_test()
         print('Loading Complete')
 
